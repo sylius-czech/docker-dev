a3275050a7e75459c29d785567e741dff56f3f66
Jaroslav Týc <mail@jaroslavtyc.com>
2023-05-23 18:18:02 +0200

    Use this only as Docker recipe

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-docker build --pull --target sylius-plugin-php --tag jaroslavtyc/praguebest-sylius-plugin-dev-php:8.0 . && docker push jaroslavtyc/praguebest-sylius-plugin-dev-php:8.0
-docker build --pull --target sylius-plugin-nginx --tag jaroslavtyc/praguebest-sylius-plugin-dev-nginx:1.24 . && docker push jaroslavtyc/praguebest-sylius-plugin-dev-nginx:1.24
+PHP_TAG='docker-registry.praguebest.cz:5000/sylius-plugin-dev-php:8.0'
+docker build --pull --target sylius-plugin-php --tag "$PHP_TAG" . && docker push "$PHP_TAG"
+
+NGINX_TAG='docker-registry.praguebest.cz:5000/sylius-plugin-dev-nginx:1.24'
+docker build --pull --target sylius-plugin-nginx --tag "$NGINX_TAG" . && docker push "$NGINX_TAG"
