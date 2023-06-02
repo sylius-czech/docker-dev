23891f902482c89727c56e0ed75b45eedfbc9e05
Jaroslav Týc <mail@jaroslavtyc.com>
2023-06-02 13:47:03 +0200

    Fix shared dir to use for host user ID detection

--- docker/node/docker-change-user-id.sh
+++ docker/node/docker-change-user-id.sh
@@ -14,11 +14,11 @@ if [ -n "${GID+x}" ] && [ "${GID}" -ne 0 ]; then
   CURRENT_GID=$GID # externally passed as an env variable
 fi
 
-CURRENT_SCRIPT_FILE=$( realpath "${BASH_SOURCE[0]}" )
-DIR_SHARED_FROM_HOST=/usr/local/sylius-plugin-dev
+CURRENT_SCRIPT_FILE=$(realpath "${BASH_SOURCE[0]}")
+DIR_SHARED_FROM_HOST=/
 
-  # Set CURRENT_UID of user on guest (inside Docker) to match the CURRENT_UID of the user on the host machine
-  # `stat -c "%u" $1` gives user(owner) of this file (expected a file inside current Docker container)
+# Set CURRENT_UID of user on guest (inside Docker) to match the CURRENT_UID of the user on the host machine
+# `stat -c "%u" $1` gives user(owner) of this file (expected a file inside current Docker container)
 if [ -z "${CURRENT_UID}" ] || [ "${CURRENT_UID}" -eq 0 ]; then
   CURRENT_SCRIPT_UID=$(stat -c "%u" "$CURRENT_SCRIPT_FILE")
   if [ "$CURRENT_SCRIPT_UID" -gt 0 ]; then
@@ -51,8 +51,7 @@ if [ -z "${CURRENT_GID}" ] || [ "${CURRENT_GID}" -eq 0 ]; then
   CURRENT_GID=1000
 fi
 
-
-usermod --uid "${CURRENT_UID}" "${REQUIRED_USER}" # allow to set UID manually from host
+usermod --uid "${CURRENT_UID}" "${REQUIRED_USER}"  # allow to set UID manually from host
 groupmod --gid "${CURRENT_GID}" "${REQUIRED_USER}" # allow to set GID manually from host
 
 # Allow user to log in to use development tools
@@ -62,7 +61,7 @@ usermod --shell /bin/bash "${REQUIRED_USER}"
 echo -e echo "Switching to user ${REQUIRED_USER}\n" \
   sudo --preserve-env su "${REQUIRED_USER}\n" \
   echo You are now "'$(whoami) $(id)'" \
-  > /root/.bashrc
+  >/root/.bashrc
 
 echo -e 'export PATH="/usr/local/bin:$PATH"' "\n" \
-  > /home/node/.bashrc
+  >/home/node/.bashrc
