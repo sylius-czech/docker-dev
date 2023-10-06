62db953aa96aa7c3f57f527c48ca1d48576e2db0
Jaroslav Týc <mail@jaroslavtyc.com>
2023-10-06 12:19:01 +0200

    Use universal tag for Nginx

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ docker build --pull --target sylius-plugin-php --tag "$PHP_TAG" .
 (docker run "$PHP_TAG" sh -c 'echo IT works even with PHP!' && docker push "$PHP_TAG") || (echo 'PHP image is broken' && exit 1)
 
 NGINX_VERSION=$(grep -oP 'ARG NGINX_VERSION=\K[0-9]+[.][0-9]+' Dockerfile)
-NGINX_TAG="docker-registry.praguebest.cz:5000/sylius-plugin-dev-nginx:${NGINX_VERSION}"
-docker build --pull --target sylius-plugin-nginx --tag "$NGINX_TAG" .
-(docker run "$NGINX_TAG" sh -c 'echo IT works even with Nginx!' && docker push "$NGINX_TAG") || (echo 'Nginx image is broken' && exit 1)
+NGINX_BASE_NAME="docker-registry.praguebest.cz:5000/sylius-plugin-dev-nginx"
+NGINX_TAG="${NGINX_BASE_NAME}:${NGINX_VERSION}"
+NGINX_LATEST_TAG="${NGINX_BASE_NAME}:latest"
+docker build --pull --target sylius-plugin-nginx --tag "$NGINX_TAG" --tag "$NGINX_LATEST_TAG" .
+(docker run "$NGINX_TAG" sh -c 'echo IT works even with Nginx!' && docker push --all-tags "${NGINX_BASE_NAME}") || (echo 'Nginx image is broken' && exit 1)
