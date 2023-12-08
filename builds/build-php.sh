799a1ef846a86e45419151f78dc9a6326ec854c1
Jaroslav Týc <mail@jaroslavtyc.com>
2023-12-08 17:00:02 +0100

    Push images to Docker hub again

--- builds/build-php.sh
+++ builds/build-php.sh
@@ -2,8 +2,8 @@
 
 PHP_VERSION=$(grep -oP 'ARG PHP_VERSION=\K[0-9]+[.][0-9]+' Dockerfile)
 PHP_BASE_NAMES=(
-  "docker-registry.praguebest.cz:5000/sylius-app-dev-php"
-  "docker-registry.praguebest.cz:5000/sylius-plugin-dev-php"
+  "jaroslavtyc/sylius-app-dev-php"
+  "jaroslavtyc/sylius-plugin-dev-php"
 )
 for PHP_BASE_NAME in ${PHP_BASE_NAMES[*]}; do
   PHP_TAG="${PHP_BASE_NAME}:${PHP_VERSION}"
